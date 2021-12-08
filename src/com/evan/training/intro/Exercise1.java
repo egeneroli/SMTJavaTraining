436bc290b48a6b8eea52d174aa40e4bbfdd12474
Evan Generoli <evan.generoli@siliconmtn.com>
2021-12-08 13:44:28 -0700

    first commit

---
+++ src/com/evan/training/intro/Exercise1.java
@@ -0,0 +1,22 @@
+package com.evan.training.intro;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		
+		// declare and initialize variables of each type (int, double, float, char, boolean)
+		int myInt = 5;
+		double myDouble = 6.987;
+		float myFloat = 5.4f;
+		char myChar = 'a';
+		boolean myBoolean = true;
+		
+		// print out each declared variable (each on a new line)
+		System.out.println(myInt);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		
+	}
+
+}
---
+++ src/com/evan/training/intro/Exercise2.java
@@ -0,0 +1,25 @@
+package com.evan.training.intro;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		
+		// print numbers 1-10 with while loop
+		int i = 0; // declare int variable called i, initialize with value of 0
+		while (i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		// print numbers 10-1 with for loop
+		for(i=10;i>0;i--) {
+			System.out.print(i);
+		}
+		
+		// print even numbers 1-20 w/ for loop
+		for(i=1;i<=20;i=i+2) {
+			System.out.print(i);
+		}
+		
+		
+	}
+}
