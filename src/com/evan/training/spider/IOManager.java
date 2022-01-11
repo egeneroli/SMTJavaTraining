a877e93896a1d542cf328ac8e1a5c7ce6c7e032f
Evan Generoli <33590952+egeneroli@users.noreply.github.com>
2022-01-11 16:57:47 -0700

    added ConnectionManager, IOManager, and HtmlParser classes

---
+++ src/com/evan/training/spider/IOManager.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.evan.training.spider;
+
+/****************************************************************************
+* <b>Title</b>: IOManager.java
+* <b>Project</b>: SMTJavaTraining
+* <b>Package</b>: com.evan.training.spider
+* <b>Description: </b> FILL IN DESCRIPTION HERE
+* 
+* @author egeneroli
+* @version 1.0
+* @since Jan 11, 2022
+* @updates:
+****************************************************************************/
+public class IOManager {
+
+}
